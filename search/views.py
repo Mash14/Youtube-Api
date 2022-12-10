52d0929f18c713dd460cdcf5d5d7066a9312f28c
Mash14 <mashalonzo741@gmail.com>
2022-12-10 13:14:41 +0300

    Initiate the youtube api

--- search/views.py
+++ search/views.py
@@ -10,6 +10,7 @@ api_key = config('YOUTUBE_API_KEY')
 
 def search(request):
     videos = list()
+    query = ''
 
     if request.method == 'POST':
         # Search the api to find the video ids 
@@ -94,5 +95,5 @@ def single_item(request,slug='video_id'):
         'time' : int(parse_duration(data['items'][0]['contentDetails']['duration']).total_seconds() // 60),
         'url' : f'https://www.youtube.com/embed/{data["items"][0]["id"]}'
     }
-
+    print(data['items'][0]['snippet']['description'])
     return render(request,'single_item.html',{'video':video})
